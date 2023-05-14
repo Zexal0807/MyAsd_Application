724ccee5175023d785e78381ed789e20ccb4ee8a
Zexal0807 <gallinar00@gmail.com>
2023-05-14 11:24:45 +0200

    Add remember session

--- app/src/main/java/com/roujutsu/myasd/MainActivity.java
+++ app/src/main/java/com/roujutsu/myasd/MainActivity.java
@@ -1,32 +1,96 @@
 package com.roujutsu.myasd;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.webkit.CookieManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private WebView myWebView;
+    private SharedPreferences sharedPreferences;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myWebView  = (WebView) findViewById(R.id.webview);
+        this.sharedPreferences = getPreferences(MODE_PRIVATE);
+
+        myWebView = (WebView) findViewById(R.id.webview);
 
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
             public void onPageFinished(WebView view, String url) {
-                CookieManager.getInstance().flush();
+                super.onPageFinished(view, url);
             }
         });
 
+
+        //        checkDownloadPermission();
+        /*
+        myWebView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+                Uri uri = Uri.parse("https://myasd.roujutsu.it/api/file/10");
+                //Uri uri = Uri.parse("https://www.roujutsu.it/frontEnd/assets/logo.png");
+
+                DownloadManager.Request request = new DownloadManager.Request(uri);
+
+                request.allowScanningByMediaScanner();
+                request.setTitle("10.png");
+                request.setDescription("Downloading file...");
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED); //Notify client once download is completed!
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "10.png");
+                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                dm.enqueue(request);
+                Toast.makeText(getApplicationContext(), "Downloading File", Toast.LENGTH_LONG).show();
+            }
+            BroadcastReceiver onComplete = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    Toast.makeText(getApplicationContext(), "Downloading Complete", Toast.LENGTH_SHORT).show();
+                }
+            };
+        });
+        */
+
         myWebView.loadUrl("https://myasd.roujutsu.it");
+        String cookie = sharedPreferences.getString("cookie", "null");
+        if (!cookie.equals("null")) {
+            CookieManager.getInstance().setCookie("https://myasd.roujutsu.it", cookie);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        String cookie = CookieManager.getInstance().getCookie("https://myasd.roujutsu.it");
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("cookie", cookie);
+        editor.commit();
+    }
+
+    private void checkDownloadPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            Toast.makeText(MainActivity.this, "Write External Storage permission allows us to save files. Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+        }
     }
 
     @Override
