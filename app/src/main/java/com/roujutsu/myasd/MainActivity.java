fa1ec199eceda8ad47d7867c61e3c9e133ef00bc
Zexal0807 <gallinar00@gmail.com>
2024-08-06 14:32:03 +0200

    Add takePicture and upload

--- app/src/main/java/com/roujutsu/myasd/MainActivity.java
+++ app/src/main/java/com/roujutsu/myasd/MainActivity.java
@@ -1,7 +1,10 @@
 package com.roujutsu.myasd;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.DownloadManager;
 import android.content.BroadcastReceiver;
@@ -11,20 +14,34 @@ import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.KeyEvent;
 import android.webkit.CookieManager;
 import android.webkit.DownloadListener;
 import android.webkit.URLUtil;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private WebView myWebView;
     private SharedPreferences sharedPreferences;
+
+    private ValueCallback<Uri[]> filePathCallback;
+    private String cameraPhotoPath;
+
+    private static final int FILECHOOSER_RESULTCODE = 1;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -32,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        checkDownloadPermission();
+        checkPermission();
 
         this.sharedPreferences = getPreferences(MODE_PRIVATE);
 
@@ -40,6 +57,64 @@ public class MainActivity extends AppCompatActivity {
 
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setPluginState(WebSettings.PluginState.ON);
+
+        myWebView.setWebChromeClient(new WebChromeClient(){
+            @RequiresApi(api = Build.VERSION_CODES.Q)
+            @Override
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+                if (MainActivity.this.filePathCallback != null) {
+                    MainActivity.this.filePathCallback.onReceiveValue(null);
+                }
+                MainActivity.this.filePathCallback = filePathCallback;
+
+                // Intent per scattare una foto
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                        takePictureIntent.putExtra("PhotoPath", cameraPhotoPath);
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+
+                    if (photoFile != null) {
+                        cameraPhotoPath = photoFile.getAbsolutePath();
+                        Uri photoURI = FileProvider.getUriForFile(MainActivity.this, BuildConfig.APPLICATION_ID + ".fileprovider", photoFile);
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    } else {
+                        takePictureIntent = null;
+                    }
+                }
+
+                // Intent per selezionare un contenuto
+                Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+                contentSelectionIntent.setType("image/*");
+
+                Intent[] intentArray;
+                if (takePictureIntent != null) {
+                    intentArray = new Intent[]{takePictureIntent};
+                } else {
+                    intentArray = new Intent[0];
+                }
+
+                // Creazione di un Intent per effettuare il chooser
+                Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+                chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+
+                // Avvia l'activity per selezionare l'immagine
+                startActivityForResult(chooserIntent, FILECHOOSER_RESULTCODE);
+
+                return true;
+            }
+        });
+
         myWebView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
@@ -91,6 +166,53 @@ public class MainActivity extends AppCompatActivity {
         myWebView.loadUrl("https://myasd.roujutsu.it");
     }
 
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        if (storageDir != null && !storageDir.exists()) {
+            storageDir.mkdirs();
+        }
+        File image = File.createTempFile(timeStamp, ".png", storageDir);
+        cameraPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == FILECHOOSER_RESULTCODE) {
+            handleFileChooserResult(resultCode, data);
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void handleFileChooserResult(int resultCode, Intent data) {
+        if (filePathCallback == null) {
+            return;
+        }
+
+        Uri[] results = null;
+
+        if (resultCode == RESULT_OK) {
+            if (data == null || data.getData() == null) {
+                // Immagine scattata dalla fotocamera
+                if (cameraPhotoPath != null) {
+                    File file = new File(cameraPhotoPath);
+                    Uri uri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".fileprovider", file);
+                    results = new Uri[]{uri};
+                }
+            } else {
+                // Apertura di un file
+                String dataString = data.getDataString();
+                if (dataString != null) {
+                    results = new Uri[]{Uri.parse(dataString)};
+                }
+            }
+        }
+
+        filePathCallback.onReceiveValue(results);
+        filePathCallback = null;
+    }
     @Override
     protected void onPause() {
         super.onPause();
@@ -101,14 +223,14 @@ public class MainActivity extends AppCompatActivity {
         editor.commit();
     }
 
-    private void checkDownloadPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            Toast.makeText(MainActivity.this, "Write External Storage permission allows us to save files. Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
-        } else {
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
-        }
-    }
+    private void checkPermission() {
+        String[] permissions = {
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+        };
 
+        ActivityCompat.requestPermissions(MainActivity.this, permissions, 100);
+    }
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (event.getAction() == KeyEvent.ACTION_DOWN) {
@@ -123,4 +245,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onKeyDown(keyCode, event);
     }
+
 }
